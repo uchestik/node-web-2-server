befbcddf419c626f8e2b012192c0fe53845fe0be
ON3\Uchenna.Nwankpa <uchenna.nwankpa@on3.net>
2018-09-14 23:22:06 -0500

    Setup start script and heroku port

--- server.js
+++ server.js
@@ -45,6 +45,8 @@ app.get('/about', (req, res) => {
     })
 })
 
-app.listen(5000, () => {
-    console.log('Server is running on Port 5000')
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
 })
